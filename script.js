3768b263b23b63c4beed98fb298a6307a88075e2
chargrace <lottie808@hotmail.co.uk>
2024-12-12 19:52:22 +0000

    debugged multiplechoice function

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ async function getData() {
     console.log(quizData);
     // Call displayData and pass quizData
     displayData(quizData);
+    //Call multipleChpice and pass quizdata
+    multipleChoice(quizData);
 }
 
 questionsList.innerHTML = "";
@@ -41,19 +43,28 @@ function displayData(quizData) {
 
 function multipleChoice (quizData) {
     for (let element of quizData.results) {
-    const answers = decodeURIComponent(element.incorrect_answers);
-    const correct = decodeURIComponent(element.correct_answer);
-    const optionalChoice = answers + correct;
-    console.log(optionalChoice);
-
+        const answers = decodeURIComponent(element.incorrect_answers);
+        const correct = decodeURIComponent(element.correct_answer);
+        const optionalChoice = answers + correct; //issue: correct answer is always at the end
+        console.log(optionalChoice);
+        
     }
 }
 
 // async function List(quizData){
-//     let questionList = document.createElement("li");
-//     questionList.textContent = quizData;
-//     document.body.appendChild(questionList)
-// }
-
-
-
+    //     let questionList = document.createElement("li");
+    //     questionList.textContent = quizData;
+    //     document.body.appendChild(questionList)
+    // }
+    
+    
+    
+    //issue solution
+    //put correct into array at random point
+    
+    //splice only works on arrays not strings, so putting correct into incorrect before decoding
+    // const encodedAnswers = element.incorrect_answers;
+    // const encodedCorrect = element.correct_answer;
+    // const choices = encodedAnswers.splice((Math.floor(Math.random()) * (encodedAnswers.length +1)), 0, encodedCorrect);//solution
+    //const decodedChoice = decodeURIComponent(choices);
+    // console.log(choices);
