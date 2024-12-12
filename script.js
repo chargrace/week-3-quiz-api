f9d4adf6af8747e881f870a235ea42dd647da93b
chargrace <lottie808@hotmail.co.uk>
2024-12-12 15:32:04 +0000

    retrieve data from api

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+//Fetch is made upon page loading
+let pageLoad = document.addEventListener("load", getData());
+
+// This function receives all of the data via JSON file.
+
 async function getData () {
-    const response = await fetch("https://arbeitnow.com/api/job-board-api", {
-        headers: {
-            "Content-Type" : "application/json"
-        }
-    });
+    const response = await fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple&encode=url3986");
     console.log(response);
-    const jobData = await response.json();
-    console.log(jobData);
+    const quizData = await response.json();
+    console.log(quizData);
+}
+
+//This function retrieves ID of each question
+function displayData (quizData) {
+ for (let questions of quizData.results.question) {
+    console.log(questions);
+ }
 }
